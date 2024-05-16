3ec774fe893f46467a2b3ded853081f537a5ca46
c01dsnap <c01dsnap.max@gmail.com>
2024-05-16 14:14:32 +0800

    Update: API for embedding

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 import numpy as np
-from typing import List
 from fastapi import FastAPI, Request, HTTPException
 from pymilvus.model.hybrid import BGEM3EmbeddingFunction
+
+import utils
 
 app = FastAPI()
 
@@ -11,17 +12,12 @@ bge_m3_ef = BGEM3EmbeddingFunction(
     use_fp16 = False # Specify whether to use fp16. Set to `False` if `device` is `cpu`.
 )
 
-def get_embedding(model, texts: List[str]):
-    text_embeddings = model.encode_documents(texts)
-    text_embeddings_dense = text_embeddings['dense']
-    return text_embeddings_dense
-
 @app.post("/embeddings")
 async def generate_embeddings(request: Request):
     try:
         json_post = await request.json()
         strings = json_post["strings"]
-        embeddings = get_embedding(bge_m3_ef, strings)
+        embeddings = utils.get_embedding(bge_m3_ef, strings)
         embeddings = np.array(embeddings).tolist()
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+from typing import List
 
-
-
+def get_embedding(model, texts: List[str]):
+    text_embeddings = model.encode_documents(texts)
+    text_embeddings_dense = text_embeddings['dense']
+    return text_embeddings_dense
